a3b13e668ab3eb2d1c0fbb7891cec6da6dc51b6d
Tushar05masne <masnetushar05@gmail.com>
2025-06-14 12:50:56 +0530

    Git desktop commit

--- src/main/java/com/test/ui/actions/BaseTest.java
+++ src/main/java/com/test/ui/actions/BaseTest.java
@@ -1,34 +1,33 @@
 package com.test.ui.actions;
 
-import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Parameters;
-
-import com.test.driverfactory.DriverFactory;
+//import org.openqa.selenium.WebDriver;
+//import org.testng.annotations.AfterTest;
+//import org.testng.annotations.BeforeTest;
+//import org.testng.annotations.Parameters;
+//
+//import com.test.driverfactory.DriverFactory;
 
 public class BaseTest {
 	
 
-	public static WebDriver driver;
-	DriverFactory df;
-	static boolean flag=true;
-	
-	
-	@Parameters("Browser")
-	@BeforeClass
-	@BeforeTest
-	public  void setUp(String Browser) {
-		df=new DriverFactory();
-		driver=df.initDriver(Browser);
-	}
-	
-	@AfterTest
-	public static void closeBrowser() {
-		// driver.close();
-		 driver.quit();
-	}
+//	public static WebDriver driver;
+//	DriverFactory df;
+//	static boolean flag=true;
+//	
+//	
+//	@Parameters("Browser")
+//	//@BeforeClass
+//	@BeforeTest
+//	public  void setUp(String Browser) {
+		//df=new DriverFactory();
+//		driver=df.initDriver(Browser);
+//	}
+//	
+//	@AfterTest
+//	public static void closeBrowser() {
+//		// driver.close();
+//		 driver.quit();
+//	}
 }
 
 
