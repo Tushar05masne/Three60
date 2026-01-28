e0bd62928935e6fb9dc1f428f2694b4cde3c88c4
Tushar05masne <masnetushar05@gmail.com>
2026-01-28 23:37:24 +0530

    Update TestClass1.java

--- src/test/java/com/tests/TestClass1.java
+++ src/test/java/com/tests/TestClass1.java
@@ -31,7 +31,7 @@ public class TestClass1 {
 		Web_Tables wt = new Web_Tables();
 		wt.getTableRows();
 		System.out.println("Hi TC2 run completed");
-	//	System.out.println("Hi TC2 run completed");
+		System.out.println("Hi TC2 run completed");
 
 
 	}
